b2013a6421139ebb897e70228b74c6b75520677d
vvaneeclo <victor.vaneecloo@devoteamgcloud.com>
2024-01-17 15:49:08 +0100

    feat/1/added_tests
    **Features**
    - Added test to create examples of using the macro.
    
    **Fixes**
    - /
    
    **Breaking Changes**
    /
    
    Issue #/

---
+++ tests/generic/re_assert_assertions.sql
@@ -0,0 +1,13 @@
+{% test assertions_test(model, column_name, blacklist, whitelist, from_column, re_assert=False) %}
+
+with validation as (
+    SELECT *,
+    {% if re_assert %}
+        {{ dbt_assertions.assertions() }}
+    {% endif %}
+    FROM {{ model }}
+)
+
+SELECT * FROM validation WHERE {{ dbt_assertions.assertions_filter(reverse=true) }}
+
+{% endtest %}
