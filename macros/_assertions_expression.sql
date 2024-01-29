12b19ba523c1b9f13dc664dca9e05f8f7df05535
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2024-01-29 10:18:08 +0100

    Add Snowflake for `assertions()` and `assertions_filter()`

--- macros/_assertions_expression.sql
+++ macros/_assertions_expression.sql
@@ -31,3 +31,33 @@ ARRAY_CONCAT(
 ) AS {{ column }}
 
 {%- endmacro %}
+
+
+{%- macro snowflake___assertions_expression(column, assertions) -%}
+
+ARRAY_CONSTRUCT_COMPACT(
+    {%- for assertion_id, assertion_config in assertions.items() %}
+
+    {%- set expression =
+        assertion_config.expression
+        if '\n' not in assertion_config.expression
+        else assertion_config.expression | indent(12) -%}
+
+    {%- set description= assertion_config.description -%}
+    {%- set null_as_exception =
+        'FALSE'
+        if (assertion_config.null_as_exception is not defined
+            or assertion_config.null_as_exception is true)
+        else 'TRUE' %}
+
+    /* {{ assertion_id }}: {{ description }} */
+    IFF(
+        COALESCE({{ expression }}, {{ null_as_exception }}) = FALSE,
+        '{{ assertion_id }}',
+        CAST(NULL AS STRING)
+    ),
+    {%- endfor %}
+    CAST(NULL AS STRING)
+) AS {{ column }}
+
+{%- endmacro %}
