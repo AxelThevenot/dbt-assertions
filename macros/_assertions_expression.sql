190344bb1768fa3feceef9766710c903bd527f30
Ferdy Hulzebos <ferdy.hulzebos@getthere.nl>
2024-11-01 09:34:38 +0100

    Added Databricks support

--- macros/_assertions_expression.sql
+++ macros/_assertions_expression.sql
@@ -89,3 +89,31 @@ LIST_DISTINCT([
 ]) AS {{ column }}
 
 {%- endmacro %}
+
+{%- macro databricks___assertions_expression(column, assertions) -%}
+
+ARRAY_DISTINCT(ARRAY(
+    {%- for assertion_id, assertion_config in assertions.items() %}
+
+    {%- set expression =
+        assertion_config.expression
+        if '\n' not in assertion_config.expression
+        else assertion_config.expression | indent(12) -%}
+
+    {%- set description= assertion_config.description -%}
+    {%- set null_as_exception =
+        'FALSE'
+        if (assertion_config.null_as_exception is not defined
+            or assertion_config.null_as_exception is true)
+        else 'TRUE' %}
+
+    /* {{ assertion_id }}: {{ description }} */
+    IF(
+        COALESCE({{ expression }}, {{ null_as_exception }}) = FALSE,
+        '{{ assertion_id }}',
+        string(null)
+    ){% if not loop.last %},{% endif %}
+    {%- endfor %}
+)) AS {{ column }}
+
+{%- endmacro %}
