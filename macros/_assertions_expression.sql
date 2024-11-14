028e3d2a57c1ccf3fdcd3e828045f4dc634bab4c
Rich Herman <rich.herman@kochcc.com>
2024-11-14 15:11:49 -0500

    Added Redshift Support

--- macros/_assertions_expression.sql
+++ macros/_assertions_expression.sql
@@ -117,3 +117,30 @@ ARRAY_DISTINCT(ARRAY(
 )) AS {{ column }}
 
 {%- endmacro %}
+
+{%- macro redshift___assertions_expression(column, assertions) -%}
+
+ARRAY_FLATTEN(
+    ARRAY(
+        {%- for assertion_id, assertion_config in assertions.items() %}
+        {%- set expression =
+            assertion_config.expression
+            if '\n' not in assertion_config.expression
+            else assertion_config.expression | indent(12) -%}
+        {%- set description= assertion_config.description -%}
+        {%- set null_as_exception =
+            'FALSE'
+            if (assertion_config.null_as_exception is not defined
+                or assertion_config.null_as_exception is true)
+            else 'TRUE' %}
+        /* {{ assertion_id }}: {{ description }} */
+        CASE WHEN COALESCE({{ expression }}, {{ null_as_exception }}) = FALSE
+            THEN ARRAY('{{ assertion_id }}')
+            ELSE ARRAY()
+        END,
+        {%- endfor %}
+        ARRAY()
+    )
+) AS {{ column }}
+
+{%- endmacro %}
