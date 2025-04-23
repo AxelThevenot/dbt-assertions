51ca9cbe73ef74828be1c68509c2fdd31462d92f
François Lenne <francois.lenne@hotmail.fr>
2025-04-23 15:01:55 +0200

    add the spark macros

--- macros/_assertions_expression.sql
+++ macros/_assertions_expression.sql
@@ -202,3 +202,31 @@ arrayFilter(x -> x IS NOT NULL, [
 ]) AS {{ column }}
 
 {%- endmacro %}
+
+{%- macro spark___assertions_expression(column, assertions) -%}
+
+ARRAY_DISTINCT(ARRAY(
+    {%- for assertion_id, assertion_config in assertions.items() %}
+
+    {%- set expression =
+        assertion_config.expression
+        if '\n' not in assertion_config.expression
+        else assertion_config.expression | indent(12) -%}
+
+    {%- set description= assertion_config.description -%}
+    {%- set null_as_exception =
+        'FALSE'
+        if (assertion_config.null_as_exception is not defined
+            or assertion_config.null_as_exception is true)
+        else 'TRUE' %}
+
+    /* {{ assertion_id }}: {{ description }} */
+    IF(
+        COALESCE({{ expression }}, {{ null_as_exception }}) = FALSE,
+        '{{ assertion_id }}',
+        string(null)
+    ){% if not loop.last %},{% endif %}
+    {%- endfor %}
+)) AS {{ column }}
+
+{%- endmacro %}
